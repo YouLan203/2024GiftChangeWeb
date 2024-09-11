386c239b0dc01438a097253d0d43f91f0557948a
YouLan203 <lanangel33@gmail.com>
2024-09-11 12:09:05 +0800

    0911update

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ server.use('/image', express.static(__dirname + '/image')); //只將某資料夾
 server.use('/css', express.static(__dirname + '/css'));
 server.use('/js', express.static(__dirname + '/js'));
 server.use('/favicon.ico', express.static(__dirname + '/favicon.ico'));
+server.use('/home.html', express.static(__dirname + '/home.html'));
+server.use('/signUp.html', express.static(__dirname + '/signUp.html'));
 
 // 建立 get method 顯示 index.html 內容
 server.get('/', (req, res) => {
