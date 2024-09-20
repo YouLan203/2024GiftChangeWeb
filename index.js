f49df74975da143fcf9ce39a0e3bf2b3acd1ee66
YouLan203 <lanangel33@gmail.com>
2024-09-20 13:23:29 +0800

    0920update

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ const server = express();
 // 預設 port
 const port = process.env.PORT || 3000
 
+const multer = require('multer');
+const path = require('path');
+// 使用 express 的 middleware 來解析 form-data
+server.use(express.json({ limit: '15mb' }));
+server.use(express.urlencoded({ limit: '15mb', extended: true }));
+
+// 設定 multer 的存放方式
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // 將上傳的文件儲存到 'image' 資料夾中
+        cb(null, path.join(__dirname, './image'));
+    },
+    filename: (req, file, cb) => {
+        // 使用 req.body 中的 user 作為檔案名的一部分
+        const user = req.body.user || 'anonymous';
+        const ext = path.extname(file.originalname); // 獲取文件的副檔名
+        const fileName = `${user}${ext}`; // 動態檔案名：user-時間戳.副檔名
+
+        cb(null, fileName); // 設定檔案名稱
+    }
+});
+
+// 創建 multer 實例
+const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } });
+
 require('dotenv').config();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 server.use(express.json());
-server.use('/',express.static('.')); //將整個server資料夾放到server上的/路徑
+server.use('/', express.static('.')); //將整個server資料夾放到server上的/路徑
 server.use('/image', express.static(__dirname + '/image')); //只將某資料夾放到server上
 server.use('/css', express.static(__dirname + '/css'));
 server.use('/js', express.static(__dirname + '/js'));
@@ -33,9 +58,9 @@ async function connectDB(DB) {
     // Create a MongoClient with a MongoClientOptions object to set the Stable API version
     const client = new MongoClient(uri, {
         serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
         }
     });
 
@@ -49,7 +74,7 @@ async function connectDB(DB) {
 }
 
 
-server.get("/getUserSelect", async(req, res) => {
+server.get("/getUserSelect", async (req, res) => {
     try {
         const [client, list] = await connectDB("PeopleList");
 
@@ -66,12 +91,12 @@ server.get("/getUserSelect", async(req, res) => {
 
 })
 
-server.post("/signUpUser", urlencodedParser, async(req, res) => {
+server.post("/signUpUser", urlencodedParser, async (req, res) => {
     try {
         const [client, list] = await connectDB("PeopleList");
-        
+
         const num = await list.countDocuments(); //抓取目前資料筆數
-        const newNum = String.fromCharCode(num+64); //資料比數+64轉換成字母
+        const newNum = String.fromCharCode(num + 64); //資料比數+64轉換成字母
         const user = req.body.user;
         const password = req.body.password;
         const nickname = req.body.nickname;
@@ -82,10 +107,40 @@ server.post("/signUpUser", urlencodedParser, async(req, res) => {
         const gift = false;
         const get = "";
 
-        const query = { num: newNum, user: user, password: password, nickname: nickname, postCode: postCode, address: address, name: name, phone: phone, get: get, gift: gift};
+        const query = { num: newNum, user: user, password: password, nickname: nickname, postCode: postCode, address: address, name: name, phone: phone, get: get, gift: gift };
         const insertResult = await list.insertOne(query);
         await client.close();
         return res.status(200).json(insertResult);
+    } catch (error) {
+        return res.status(500).json({
+            result: null
+        })
+    }
+
+})
+
+server.post("/updateUser",  urlencodedParser, upload.single('file'), async (req, res) => {
+    try {
+        const [client, list] = await connectDB("PeopleList");
+        
+        const user = req.body.user;
+        const password = req.body.password;
+        const nickname = req.body.nickname;
+        const postCode = req.body.postCode;
+        const address = req.body.address;
+        const name = req.body.name;
+        const phone = req.body.phone;
+        const gift = Boolean(req.body.gift);
+        const get = "";
+
+        const search = { user: user };
+        const query = { $set: { password: password, nickname: nickname, postCode: postCode, address: address, name: name, phone: phone, get: get, gift: gift } };
+        const updateResult = await list.updateOne(search, query);
+        await client.close();
+
+        const file = req.file;
+        // console.log(file.filename);
+        return res.status(200).json(updateResult);
     } catch (error) {
         return res.status(500).json({
             result: null
--- js/scripts.js
+++ js/scripts.js
@@ -39,8 +39,78 @@ function go() {
     window.alert("尚未到抽獎時間！");
 }
 
-function save() {
-    window.alert("保存成功！");
+async function save() {
+    const user = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const nickname = document.getElementById("id").value;
+    const postCode = document.getElementById("localNum").value;
+    const address = document.getElementById("address").value;
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phoneNum").value;
+    const file = document.getElementById("giftCheck");
+    const gift = true;
+
+    const giftFile = file.files[0];
+    if (!giftFile) {
+        const url = urlHeader + `/getUserSelect?user=${user}`;
+        const res = await fetch(url, {
+            method: 'GET',
+        })
+        const personData = await res.json();
+        const check = personData.gift;
+        if (!check){
+            gift = false;
+        }
+        else{
+            const url = urlHeader + '/updateUser';
+            const headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+            const body = {
+                "user": user,
+                "password": password,
+                "nickname": nickname,
+                "postCode": postCode,
+                "address": address,
+                "name": name,
+                "phone": phone,
+                "gift": true
+            }
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(body)
+            })
+            const updateResult = await res.json();
+            console.log(updateResult);
+        }
+    }
+    else {
+        const url = urlHeader + '/updateUser';
+        const formData = new FormData();
+
+        // 將普通字段添加到 FormData
+        formData.append("user", user);
+        formData.append("password", password);
+        formData.append("nickname", nickname);
+        formData.append("postCode", postCode);
+        formData.append("address", address);
+        formData.append("name", name);
+        formData.append("phone", phone);
+        formData.append("gift", gift);
+
+        // 將文件添加到 FormData
+        formData.append("file", giftFile);
+        // 使用 fetch 發送 FormData
+        const res = await fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        const updateResult = await res.json();
+        console.log(updateResult);
+    }
+    // window.alert("保存成功！");
 }
 
 async function build() {
@@ -58,12 +128,12 @@ async function build() {
             method: 'GET',
         })
         const personData = await res.json();
-        if (personData != null){
+        if (personData != null) {
             window.alert("帳號已存在，請重新設定。");
             document.getElementById("username").value = "";
             document.getElementById("password").value = "";
         }
-        else{
+        else {
             const url = urlHeader + '/signUpUser';
             const headers = {
                 "Content-Type": "application/json",
@@ -84,7 +154,6 @@ async function build() {
                 body: JSON.stringify(body)
             })
             const insertResult = await res.json();
-            // console.log(typeof(insertResult.acknowledged));
             if (insertResult.acknowledged == true) {
                 const url = urlHeader + `/getUserSelect?user=${user}`;
                 const res = await fetch(url, {
